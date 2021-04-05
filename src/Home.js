62d3eb8f0dc6fc8703f8ae9c98d05ea299c9c86c
LAKSHAY SHARMA <lakshayne@gmail.com>
2021-04-05 22:02:29 +0530

    added home image with gradient

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.css'
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Wireless/Samsung/SamsungM/M12/GW_April/True48MP/D21200796_IN_WLME_SamsungGalaxy_M12_New_Launch_DesktopTallHero_3000x1200_6._CB655658906_.jpg" alt="top img" />
+            </div>
+        </div>
+    )
+}
+
+export default Home
